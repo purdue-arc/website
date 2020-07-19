7573b368c0b676b3e8658e85c4cd9bce3bc37324
James Baxter <baxterjr@outlook.com>
2020-07-19 00:10:01 -0400

    update docker config
    - use volume to cache gems
    - fix networking issue when using WSL2
    - use jekyll version consistent with version used by GitHub
    - add config file rather than modifying comments

--- rundocker.sh
+++ rundocker.sh
@@ -2,14 +2,19 @@
 
 if [ "$1" == "bash" ]; then
 	docker run --rm \
-  	--volume="$PWD:/srv/jekyll" \
+	-e "JEKYLL_ENV=docker" \
+  	-v "$PWD:/srv/jekyll" \
+	-v "jekyll_bundle_cache:/usr/local/bundle" \
   	-p 4000:4000 \
-  	-it jekyll/jekyll \
+  	-it jekyll/jekyll:3.8 \
   	bash
 else
 	docker run --rm \
-  	--volume="$PWD:/srv/jekyll" \
+	-e "JEKYLL_ENV=docker" \
+  	-v "$PWD:/srv/jekyll" \
+	-v "jekyll_bundle_cache:/usr/local/bundle" \
   	-p 4000:4000 \
-  	-it jekyll/jekyll \
-  	jekyll serve
+  	-it jekyll/jekyll:3.8 \
+  	jekyll serve \
+	--config _config.yml,_config.docker.yml
 fi
