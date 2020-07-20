e1b42dc41ca1cf2f84c4d3a92f08291eb45b7550
Harrison McCarty <hmccarty@beckman.com>
2020-07-20 18:15:54 -0400

    why did I forget to pull from master aghh

--- rundocker.sh
+++ rundocker.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-if [ "$1" == "bash" ]; then
-	docker run --rm \
-	-e "JEKYLL_ENV=docker" \
-  	-v "$PWD:/srv/jekyll" \
-	-v "jekyll_bundle_cache:/usr/local/bundle" \
-  	-p 4000:4000 \
-  	-it jekyll/jekyll:3.8 \
-  	bash
-else
-	docker run --rm \
-	-e "JEKYLL_ENV=docker" \
-  	-v "$PWD:/srv/jekyll" \
-	-v "jekyll_bundle_cache:/usr/local/bundle" \
-  	-p 4000:4000 \
-  	-it jekyll/jekyll:3.8 \
-  	jekyll serve \
-	--config _config.yml,_config.docker.yml
-fi
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  -p 4000:4000 \
+  -it jekyll/jekyll \
+  jekyll serve
