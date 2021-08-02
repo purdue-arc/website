cdbb30470abe16e8a160e4b107da0acea2b5e5c7
raghavauppuluri13 <raghavauppuluri13@users.noreply.github.com>
2021-08-02 16:21:23 +0000

    jekyll build from Action 1453242cc153245d90feec8653a99444c5b16dde

---
+++ rundocker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" == "bash" ]; then
+	docker run --rm \
+	-e "JEKYLL_ENV=docker" \
+  	-v "$PWD:/srv/jekyll" \
+	-v "jekyll_bundle_cache:/usr/local/bundle" \
+	-v "jekyll_gems:/usr/gem" \
+  	-p 4000:4000 \
+  	-it jekyll/jekyll:4.0 \
+  	bash
+else
+	docker run --rm \
+	-e "JEKYLL_ENV=docker" \
+  	-v "$PWD:/srv/jekyll" \
+	-v "jekyll_bundle_cache:/usr/local/bundle" \
+	-v "jekyll_gems:/usr/gem" \
+  	-p 4000:4000 \
+  	-it jekyll/jekyll:4.0 \
+  	jekyll serve \
+	--config _config.yml,_config.docker.yml
+fi
